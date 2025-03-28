6492337e8a26d80f93e17a86bc0c83494a74abcd
Vipin Kamaraju <vipinkamaraju@gmail.com>
2025-03-28 15:15:51 -0400

    dynamic path update of med_mon

--- run_med_mon.sh
+++ run_med_mon.sh
@@ -2,12 +2,14 @@
 
 xhost +local: # Allows the container to connect to the X server
 
+REPO_DIR=$(dirname "$(realpath "$0")")
+
 docker run -it --rm \
     --name med_mon_app \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v /dev/dri:/dev/dri \
     -v /dev/video0:/dev/video0 \
-    -v {ABSOLUTE_PATH_TO_MEDMON_DIR}:/med_mon \
+    -v "$REPO_DIR":/med_mon \
     -e DISPLAY=$DISPLAY \
     -e LIBGL_ALWAYS_INDIRECT=1 \
     --net=host \
